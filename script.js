0183b0e3612c9232fd232332c2e2b080721ea89b
gabrielcabrera17 <gabrielbaez023@gmail.com>
2024-04-25 16:01:49 -0400

    Vistra previa videoLab

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("page loaded...");
+function reproducirVideo(video){
+    video.play();
+    console.log("Reproduciendo");
+}
+function pausarVideo(video){
+    video.pause();
+    console.log("Pausando");
+}
